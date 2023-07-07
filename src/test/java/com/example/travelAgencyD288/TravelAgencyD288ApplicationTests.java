8aa8abdc905efb339d1a6d7c15158ddfd5575058
Amaud Simon <asimo29@wgu.edu>
2023-07-07 08:04:33 -0500

    Create New Java project and connect to git lab repository

---
+++ src/test/java/com/example/travelAgencyD288/TravelAgencyD288ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.travelAgencyD288;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelAgencyD288ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
